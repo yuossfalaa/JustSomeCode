7886231bf2dfbd1e1fbe10e59ec1225f45eb6f0e
yuossfalaa <yuossfalla@gmail.com>
2022-12-04 17:37:11 +0200

    Add Drawing Control To UI

--- JustSomeCode/ViewModels/MainViewModel.cs
+++ JustSomeCode/ViewModels/MainViewModel.cs
@@ -3,48 +3,25 @@ using System.Collections.ObjectModel;
 using System.Windows.Media;
 using System.Windows.Shapes;
 using System.Windows;
+using MVVM;
 
 namespace JustSomeCode.ViewModels
 {
-    public class MainViewModel : ObeservableObject 
+    public class MainViewModel : ViewModelBase 
     {
-        private System.Windows.Media.Color _color;
-        ObservableCollection<Polygon> _polygon;
+        //private System.Windows.Media.Color _color;
 
         public SceneViewModel SceneViewModel { get; private set; }
         public Size VisibleSize { get; set; }
         public MainViewModel() 
         {
-            Color = (Color)ColorConverter.ConvertFromString("white");
             var scene = new Scene();
             scene.AddNewLayer();
             SceneViewModel = new SceneViewModel(scene);
 
         }
-        public System.Windows.Media.Color Color
-        {
-            get
-            {
-                return _color;
-            }
-            set
-            {
-                _color = value;
-                OnPropertyChanged("Color");
-            }
-        }
-        ObservableCollection<Polygon> Polygon
-        {
-            get
-            {
-                return _polygon;
-            }
-            set
-            {
-                _polygon = value;
-                OnPropertyChanged("Shapes");
-            }
-        }
+   
+        
 
 
 
