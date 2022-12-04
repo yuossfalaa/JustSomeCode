a8df2fea8c342ffe5bd67599c30f711c5e8c2f85
yuossfalaa <yuossfalla@gmail.com>
2022-12-04 16:22:52 +0200

    Adjusting Models

---
+++ JustSomeCode/Models/Shapes/Rectangle.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace JustSomeCode.Models
+{
+    public class Rectangle
+    {
+        public List<Point> RectanglePoints {get; set; }
+
+    }
+}
