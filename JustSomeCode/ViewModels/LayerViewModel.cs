9809a901cc7b354c1362b7d88c55ca5f1ea72e74
yuossfalaa <yuossfalla@gmail.com>
2022-12-04 16:25:35 +0200

    Adding Scene and Layer ViewModel

---
+++ JustSomeCode/ViewModels/LayerViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Media.Imaging;
+using MVVM;
+using JustSomeCode.Models;
+using JustSomeCode.Services.DrawingServices;
+
+namespace JustSomeCode.ViewModels
+{
+    /// <summary>
+    /// View model for layer
+    /// </summary>
+    public class LayerViewModel:ViewModelBase
+    {
+        private readonly Layer _layer;
+        private string _name;
+
+        /// <summary>
+        /// Gets image from layer
+        /// </summary>
+        public BitmapSource Image
+        {
+            get
+            {
+                return _layer.Bitmap.BitmapToBitmapSource();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets layer name
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+            set
+            { 
+                _name = value; 
+                RaisePropertyChanged("Name");
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets visibility of the layer
+        /// </summary>
+        public bool IsVisible
+        {
+            set { _layer.IsVisible = value; }
+            get { return _layer.IsVisible; }
+        }
+
+
+        public LayerViewModel(Layer layer)
+        {
+            if (layer==null)
+                throw new ArgumentNullException("layer");
+            _layer = layer;
+            _layer.LayerChanged += _layer_LayerChanged;
+        }
+
+        private void _layer_LayerChanged(object sender, EventArgs e)
+        {
+            RaisePropertyChanged("Image");
+        }
+    }
+}
--- JustSomeCode/ViewModels/MainViewModel.cs
+++ JustSomeCode/ViewModels/MainViewModel.cs
@@ -1,20 +1,54 @@
-﻿using INStore.Models;
-using JustSomeCode.Commands;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using JustSomeCode.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Media;
+using System.Windows.Shapes;
 using System.Windows;
-using System.Windows.Media.Animation;
 
 namespace JustSomeCode.ViewModels
 {
     public class MainViewModel : ObeservableObject 
     {
+        private System.Windows.Media.Color _color;
+        ObservableCollection<Polygon> _polygon;
+
+        public SceneViewModel SceneViewModel { get; private set; }
+        public Size VisibleSize { get; set; }
         public MainViewModel() 
         {
+            Color = (Color)ColorConverter.ConvertFromString("white");
+            var scene = new Scene();
+            scene.AddNewLayer();
+            SceneViewModel = new SceneViewModel(scene);
+
         }
+        public System.Windows.Media.Color Color
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                _color = value;
+                OnPropertyChanged("Color");
+            }
+        }
+        ObservableCollection<Polygon> Polygon
+        {
+            get
+            {
+                return _polygon;
+            }
+            set
+            {
+                _polygon = value;
+                OnPropertyChanged("Shapes");
+            }
+        }
+
+
+
+     
         
     }
 }
