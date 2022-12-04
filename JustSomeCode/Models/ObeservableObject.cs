bfe15d41a4da8df87c790bcbf8dc6c5e3ed66c28
yuossfalaa <yuossfalla@gmail.com>
2022-12-04 16:23:44 +0200

    Adding Layering and Scene For the Draw control

--- JustSomeCode/Models/ObeservableObject.cs
+++ JustSomeCode/Models/ObeservableObject.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace INStore.Models
+namespace JustSomeCode.Models
 {
     public class ObeservableObject : INotifyPropertyChanged
     {
